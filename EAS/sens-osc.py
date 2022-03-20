9ff05b0181473e87ad3e88e32844e7089932339e
Clay Chaplin <clay@calarts.edu>
2022-03-20 16:52:50 -0700

    Add files via upload

---
+++ EAS/sens-osc.py
@@ -0,0 +1,22 @@
+import time
+from gpiozero import MCP3008
+from gpiozero import Button
+from pythonosc import udp_client
+
+pc = MCP3008(0)
+pot1 = MCP3008(1)
+pot2 = MCP3008(2)
+button1 = Button(17)
+button2 = Button(27)
+
+ip = "192.168.0.108"
+port = 5000
+
+client = udp_client.SimpleUDPClient(ip, port)
+
+while True:
+    client.send_message("/photocell", pc.value)
+    client.send_message("/pot1", pot1.value)
+    client.send_message("/but1", button1.value)
+    client.send_message("/but2", button2.value)
+    time.sleep(.02)
